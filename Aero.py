54746470335f4e17ba53ce0702426fe368c6e165
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-10-18 00:43:26 -0500

    1

--- Aero.py
+++ Aero.py
@@ -1,5 +1,4 @@
 import os, math
-import matplotlib as mp
 import isacalc as isa
 
 os.system("cls" if os.name == "nt" else "clear")
@@ -14,8 +13,7 @@ WingArea = 1.08236  # m^2
 SWetWing = 0.82636  # m^2, area of wing exposed to stream lines
 Sweep = math.radians(0)  # degrees, converted to radians, only non 0 when transsonic +
 AverageChord = WingArea / Wingspan
-ARD = (4/math.tan(math.radians(90-60.7)))
-print(ARD)
+ARD = 4 / math.tan(math.radians(90 - 60.7))
 AR = (Wingspan ** 2) / WingArea
 XC = 0.25  # Location of center of mass in % of chord
 TC = 0.20  # Tip thickness in % of chord NACA 0020
@@ -35,16 +33,17 @@ F = FuselageLength / (0.8 * 0.2 * 0.8)
 Gamma = 1.4  # Ratio of specific heats for dry air
 R = 287  # J/kgK specific gas constant for dry air
 E = 0.85
-Oswald = 1.78 * (1 - 0.045 * (AR) ** 0.68) - 0.64
-A0 = 0.104
-A = A0 / ((1 + (57.3 * A0)) / (math.pi * E * AR))
-Alpha = 17.5  # degrees
+Oswald = 1.78 * (1 - 0.045 * (ARD) ** 0.68) - 0.64
+A0 = 0.0941
+Cd0 = 0.007221496
+A = A0 / (1 + ((57.3 * A0) / (math.pi * E * ARD)))
+Alpha = 18.2  # degrees
 AlphaL0 = 0  # degrees
-K = (math.pi * Oswald * AR) ** (-1)
+K = (math.pi * Oswald * ARD) ** (-1)
 
 # Altitude and speed variables
 
-Altitude = 8000 / 3.28084  # Divide by 3.28084 for ft to m conversion
+Altitude = 2000  # m
 Velocity = 60  # m/s
 
 Atmosphere = isa.get_atmosphere()
@@ -53,13 +52,16 @@ Temp, Pressure, Density, SpeedSound, DynamicViscosity = isa.calculate_at_h(
 )
 
 Mach = Velocity / SpeedSound
-print(Mach)
 
 # Reynolds number
 
 ReWing = (Density * Velocity * AverageChord) / DynamicViscosity
-print(ReWing)
 ReFuse = (Density * Velocity * FuselageLength) / DynamicViscosity
+
+# Weight
+SurfaceArea = 2.809 # m^2
+Mass = (((SurfaceArea * .001) * 1930) + .3 + .0158 + .5) # kg
+Weight = Mass * 9.81
 
 # Lift
 
@@ -123,4 +125,38 @@ CD = CD0 + K * (CL ** 2)
 
 
 # Display output
-print("")
+print(f"Altiutude {Altitude} meters")
+print(f"Mach {Mach}")
+print(f"Mass {Mass} kg")
+print(f"Weight {Weight} N")
+print(f"Cl {A0}")
+print(f"Cd {Cd0}")
+print(f"CL {CL}")
+print(f"CD {CD}")
+
+
+# Efficency Ratios
+
+""" 
+Max Endurance prop ((Cl^(3/2))/Cd)max
+
+VInf = sqrt((2 * W/RhoInf * S) / (k / 3 * CD0))
+Optimal Value = 3^(3/4) / (4 * (CD0^(1/4)) * (k)^(3/4))
+
+
+Max Range Prop
+Max Endurance Jet
+
+VInf = sqrt((2 * W/RhoInf * S) / (k / CD0))
+Optimal Value = sqrt(1 / (4 * k * CD0))
+
+
+Max Range Jet
+
+VInf = sqrt((2 * W/RhoInf * S) / (3 * k / CD0))
+Optimal Value = (3/4) * (1 / (3 * k * CD0^3))^(1/4)
+
+"""
+
+CL32CDMax = (3**(3/4)) / (4 * (CD0**(1/4) * k**(3/4)))
+CLCDMax = math.sqrt(1 / (4 * k * CD0))
